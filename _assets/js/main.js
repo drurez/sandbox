1e1ceefcf80960b606d9d289a6de03db6135e217
Juarez <juarax2@gnwmag.ghc.org>
2018-04-05 11:17:04 -0700

    add grid and container toggle

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -1,11 +1,19 @@
 
 
-// toggle grid
+
 $(function(){
+	// toggle grid
 	$('.toggle-grid').click(function(){
 	  if ( $('.grid-overlay').css('visibility') == 'hidden' )
 	    $('.grid-overlay').css('visibility','visible')
 	  else
 	    $('.grid-overlay').css('visibility','hidden');
+	});
+	// toggle bootstrap container type
+	$('.toggle-container').click(function(){
+		if ( $('*#js-bs-container').hasClass('container'))
+			$('*#js-bs-container').removeClass('container').addClass('container-fluid')
+		else if ($('*#js-bs-container').hasClass('container-fluid'))
+			$('*#js-bs-container').removeClass('container-fluid').addClass('container')
 	});
 });
